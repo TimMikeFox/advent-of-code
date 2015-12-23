ccb55795f495c9645439aba8ef226cf6bea2f212
Timothy Fox <tim.mike.fox@gmail.com>
2015-12-23 16:43:36 -0500

    completed day two part two

--- 2/calculate_wrapping.rb
+++ 2/calculate_wrapping.rb
@@ -2,8 +2,8 @@
 total = 0
 STDIN.each_line do |line|
     l, w, h = line.split('x').map! { |x| x.to_i }
-    sides = [ 2*l*w, 2*w*h, 2*h*l ]
-    sides << (sides.min / 2)
-    total += sides.reduce :+
+    sides = [ 2*(l+w), 2*(w+h), 2*(h+l) ]
+    volume = l * w * h
+    total += sides.min + volume
 end
 puts total
