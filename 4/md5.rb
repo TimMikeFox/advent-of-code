65c81523f2a7b6fc0e7db7defe1fa997e881a9d6
Timothy Fox <tim.mike.fox@gmail.com>
2015-12-24 16:56:21 -0500

    modified to use lazy list enumeration

--- 4/md5.rb
+++ 4/md5.rb
@@ -2,11 +2,12 @@
 
 require 'digest'
 
-prefix = 'iwrupvqb'
-
-(1..Float::INFINITY).each do |x|
-    if Digest::MD5.hexdigest(prefix + x.to_s).start_with? "00000"
-        puts x 
-        break
-    end
+def condition(x)
+    prefix = 'iwrupvqb'
+    Digest::MD5.hexdigest(prefix + x.to_s).start_with? "00000"
 end
+
+puts (1..Float::INFINITY)
+        .lazy
+        .select{ |x| condition x }
+        .first
