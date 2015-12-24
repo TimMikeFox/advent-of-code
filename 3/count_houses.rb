c1b9122846594695c7a0fdca2ba4e9d2d89b0804
Timothy Fox <tim.mike.fox@gmail.com>
2015-12-23 21:32:00 -0500

    Added revisit logic. Day three part one finished

--- 3/count_houses.rb
+++ 3/count_houses.rb
@@ -1,6 +1,23 @@
 #!/usr/bin/env ruby
+require 'set'
+
 count = 1
+location = [0,0]
+visited_locations = Set.new
+visited_locations << location
 STDIN.each_char do |c|
-    count += 1 if ['<','>','^','v'].include? c
+    x, y = location
+    case c
+    when '<'
+        x -= 1
+    when '>'
+        x += 1
+    when '^'
+        y += 1
+    when 'v'
+        y -= 1
+    end
+    location = [x,y]
+    visited_locations << location
 end
-puts count
+puts visited_locations.count
